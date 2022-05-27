6e0414c3367bcd86be352dae70e10bda3d2eef10
goldfloored <goldfloored@gmail.com>
2022-05-28 00:40:56 +0100

    fix

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,9 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -19,8 +18,26 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+
 void binary_tree_print(const binary_tree_t *);
-typedef struct binary_tree_s heap_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+typedef struct binary_tree_s heap_t;
 heap_t *heap_insert(heap_t **root, int value);
-#endif
+heap_t *Insert_Queue(heap_t *root, int value);
+
+/**
+* struct queue_s - Queue node.
+* @node: Heap node stored.
+* @next: Pointer to next node.
+*/
+
+typedef struct queue_s
+{
+	heap_t *node;
+	struct queue_s *next;
+} queue;
+
+queue *push(queue *last, heap_t *node);
+
+#endif /* _BINARY_TREES_H_ */
