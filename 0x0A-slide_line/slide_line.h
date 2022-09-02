e0a4431c1f79a9ea3edfc5ae3442456e359ae34d
goldfloored <goldfloored@gmail.com>
2022-09-02 09:18:20 +0100

    slide_line

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -2,33 +2,12 @@
 #define SLIDE_LINE_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define SLIDE_LEFT	-1
-#define SLIDE_RIGHT	 1
-
-#define SWAP()							\
-	do {							\
-		if (merged && !line[i] && !line[i - direction])	\
-			break;					\
-		if (line[i])					\
-		{						\
-			line[empt] ^= line[i];			\
-			line[i] ^= line[empt];			\
-			line[empt] ^= line[i];			\
-			empt -= direction;			\
-		}						\
-	} while (0)
-
-#define MERGE()								\
-	do {								\
-		if (line[i] == line[i - direction])			\
-		{							\
-			line[i] += line[i - direction];			\
-			line[i - direction] ^= line[i - direction];	\
-			i -= direction;					\
-		}							\
-	} while (0)
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 2
 
 int slide_line(int *line, size_t size, int direction);
 
-#endif /* SLIDE_LINE_H */
+#endif
