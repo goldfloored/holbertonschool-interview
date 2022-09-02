f1ca705b36c97b3f6f3e60f9f6fb68a6f6edd58e
goldfloored <goldfloored@gmail.com>
2022-09-02 09:24:16 +0100

    com

--- 0x0A-slide_line/slide_line.c
+++ 0x0A-slide_line/slide_line.c
@@ -1,12 +1,101 @@
 #include "slide_line.h"
-
 /**
-*slide_line - a function that slides and merges an array of integers
-*@line: points to an array of integers containing size elements
-*@size: elements of array
-*@direction: direction can be either: SLIDE_LEFT, SLIDE_RIGHT
-*Return: 1 upon success, or 0 upon failure
+* to_right - move to the right
+* @line: pointer to head of line
+* @size: size of the int array
 */
+void to_right(int *line, size_t size)
+{
+	int i, pos = 0, max = size - 1, n = 0;
+
+	for (i = max; i >= 0; i--)
+	{
+		if (line[i] == 0)
+			pos++;
+		if (line[i] > 0)
+			n++;
+		if (line[i] > 0 && pos > 0)
+		{
+			line[i + pos] = line[i];
+			line[i] = 0;
+			i = i + pos;
+			pos = 0;
+		}
+	}
+	pos = max;
+	for (i = max; i > max - n; i--)
+	{
+		if (i > max - n + 1 && line[i] == line[i - 1])
+		{
+			line[pos] = line[i] + line[i - 1];
+			line[i - 1] = 0;
+			if (pos != i)
+				line[i] = 0;
+			pos--;
+		}
+		else if (line[i] > 0 && pos < max)
+		{
+			line[pos] = line[i];
+			if (pos != i)
+				line[i] = 0;
+			pos--;
+		}
+		else if (line[i] > 0)
+			pos--;
+	}
+}
+/**
+* to_left - move to the left
+* @line: pointer to head of line
+* @size: size of the int array
+*/
+void to_left(int *line, size_t size)
+{
+	int i, pos = 0, n = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (line[i] == 0)
+			pos++;
+		if (line[i] > 0)
+			n++;
+		if (line[i] > 0 && pos > 0)
+		{
+			line[i - pos] = line[i];
+			line[i] = 0;
+			i = i - pos;
+			pos = 0;
+		}
+	}
+	pos = 0;
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1 && line[i] == line[i + 1])
+		{
+			line[pos] = line[i] + line[i + 1];
+			line[i + 1] = 0;
+			if (pos != i)
+				line[i] = 0;
+			pos++;
+		}
+		else if (line[i] > 0 && pos > 0)
+		{
+			line[pos] = line[i];
+			if (pos != i)
+				line[i] = 0;
+			pos++;
+		}
+		else if (line[i] > 0)
+			pos++;
+	}
+}
+/**
+ * slide_line - slides and merges an array of integers
+ * @line: pointer to head of line
+ * @size: size of the int array
+ * @direction: direction to slide
+ * Return: 1 if accomplish, 0
+ */
 int slide_line(int *line, size_t size, int direction)
 {
 	if (direction != 0 && direction != 1)
@@ -14,72 +103,8 @@ int slide_line(int *line, size_t size, int direction)
 	if (line == NULL || size < 1)
 		return (0);
 	if (direction == 0)
-		direction_left(line, size);
+		to_left(line, size);
 	if (direction == 1)
-		direction_right(line, size);
+		to_right(line, size);
 	return (1);
-}
-
-/**
- * direction_left - slides a line to the left
- * @line: input array
- * @size: size of array
- */
-void direction_left(int *line, size_t size)
-{
-	int i = 0, j, prev = 0, tmp;
-
-	for (j = 0; j < (int)size; j++)
-	{
-		tmp = line[j];
-		if (!tmp)
-			continue;
-		if (!prev)
-			prev = tmp;
-		else if (prev == tmp)
-		{
-			line[i++] = tmp << 1;
-			prev = 0;
-		} else
-		{
-			line[i++] = prev;
-			prev = tmp;
-		}
-	}
-	if (prev)
-		line[i++] = prev;
-	while (i < (int)size)
-		line[i++] = 0;
-}
-
-/**
- * direction_right - slides a line to the right
- * @line: input array
- * @size: size of array
- */
-void direction_right(int *line, size_t size)
-{
-	int prev = 0, i = size - 1, j, tmp;
-
-	for (j = size - 1; j >= 0; j--)
-	{
-		tmp = line[j];
-		if (!tmp)
-			continue;
-		if (!prev)
-			prev = tmp;
-		else if (prev == tmp)
-		{
-			line[i--] = tmp << 1;
-			prev = 0;
-		} else
-		{
-			line[i--] = prev;
-			prev = tmp;
-		}
-	}
-	if (prev)
-		line[i--] = prev;
-	while (i >= 0)
-		line[i--] = 0;
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,16 +1,12 @@
-#ifndef SLIDE_LINE
-#define SLIDE_LINE
+#ifndef SLIDELINE_H
+#define SLIDELINE_H
 
-/**##### MACROS ######*/
-#define SLIDE_LEFT 0
-#define SLIDE_RIGHT 1
-
-/**###### LIBS USED ######*/
 #include <stdlib.h>
 #include <stdio.h>
 
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
 int slide_line(int *line, size_t size, int direction);
-void direction_right(int *line, size_t size);
-void direction_left(int *line, size_t size);
 
 #endif
