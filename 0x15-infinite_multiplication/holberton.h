b9362059cedded28748b46f8ce101bea378b3d7e
goldfloored <goldfloored@gmail.com>
2022-09-23 10:02:23 +0000

    com

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+#endif /*HOLBERTON_H*/
