99421e2d847fd782e63eb2ea0703ba9e2af6cc98
goldfloored <goldfloored@gmail.com>
2022-10-03 11:02:43 +0000

    com

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
-"""
-Count the number of appearances of keywords within
-reddit hot titles
-"""
+""" Reddit hot article word count """
 import requests
-from collections import Counter, defaultdict
 
 
-def count_words(subreddit, wordlist, count=None, after=None):
-    """
-    make a request to reddit hot titles and check number of
-    appearances of keyword
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'
-    response = requests.get(
-            url.format(subreddit),
-            headers={'User-Agent': 'Mozilla/5.0'},
-            params={'after': after, 'limit': 100},
-            allow_redirects=False,
-        )
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if not count:
-            count = defaultdict(int)
-        page = Counter(w for s in data.get('children')
-                       for w in s.get('data').get('title').casefold().split())
-        for each in wordlist:
-            if each.casefold() in page:
-                count[each.casefold()] += page.get(each.casefold())
-        if data.get('after'):
-            count_words(subreddit, wordlist, count, data.get('after'))
-        else:
-            for k, v in sorted(sorted(count.items(),
-                               key=lambda x: x[0]), key=lambda x: x[1],
-                               reverse=True):
-                print(f"{k}: {v}")
+def count_words(subreddit, word_list, after='', words_counting={}):
+    """ Word counting function """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'custom'}
+    payload = {'limit': '100', 'after': after}
+    response = requests.get(url, headers=headers,
+                            params=payload, allow_redirects=False)
+
+    if not response.status_code == 200:
+        return
+    data = response.json().get('data')
+    after = data.get('after')
+    children_list = data.get('children')
+
+    for child in children_list:
+        title = child.get('data').get('title')
+        for word in word_list:
+            ocurrences = title.lower().split().count(word.lower())
+            if ocurrences > 0:
+                if word in words_counting:
+                    words_counting[word] += ocurrences
+                else:
+                    words_counting[word] = ocurrences
+
+    if after is not None:
+        return count_words(subreddit, word_list, after, words_counting)
+    else:
+        if not len(words_counting) > 0:
+            return
+        iterator = sorted(words_counting.items(),
+                          key=lambda kv: (-kv[1], kv[0]))
+        for key, value in iterator:
+            print('{}: {}'.format(key.lower(), value))
