0ccead7d2769448efdbca483c58e1e18bf098443
goldfloored <goldfloored@gmail.com>
2022-10-26 07:54:33 +0000

    com

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-""" make change algo """
+"""0x19-Making Change."""
 
 
 def makeChange(coins, total):
-    """ make change algo """
-    coins.sort(reverse=True)
-    num = 0
-    for each in coins:
-        num += total // each
-        total = total % each
-    if not total:
-        return num
-    return -1
+    """Change comes from within."""
+    pocketChange = 0
+    myCoins = 0
+    coins = reversed(sorted(coins))
+
+    if total <= 0:
+        return (0)
+
+    for coin in coins:
+        while pocketChange + coin <= total:
+            myCoins += 1
+            pocketChange += coin
+
+    if pocketChange != total:
+        return (-1)
+    return (myCoins)
